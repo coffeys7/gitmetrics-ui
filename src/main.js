0042cab86f7c5739fe8b37eb367e1afdec4221e5
Sam Coffey <coffey.b.sam@gmail.com>
2019-01-20 16:25:17 -0500

    Update main.js imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
 import ElementUI from 'element-ui';
 import VueResource from 'vue-resource/dist/vue-resource';
 import locale from 'element-ui/lib/locale/lang/en';
 Vue.use(ElementUI, { locale });
 Vue.use(VueResource);
+import App from './App.vue'
+Vue.use(require('vue-moment'));
 
 Vue.config.productionTip = false
 
