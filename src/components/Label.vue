3fc65a0100be7463b97b21df70f55a61cac9a91d
Sam Coffey <coffey.b.sam@gmail.com>
2019-01-21 00:16:36 -0500

    Create label component

---
+++ src/components/Label.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="label xsm-text" :style="style">
+    {{label.name}}
+  </span>
+</template>
+
+<script>
+
+import { Mixins } from '../mixins';
+
+export default {
+  name: 'Label',
+  mixins: [
+    Mixins.color
+  ],
+  props: {
+    label: Object
+  },
+  methods: {
+    style(label) {
+      const that = this;
+      let styles = {
+        'background-color': `#${label.color}`,
+        'color': that.getContrastText(label.color)
+      };
+      return _.reduce(_.keys(styles), (str, key) => {
+        return `${str}${key}:${styles[key]};`;
+      }, '');
+    }
+  }
+}
+
+</script>
